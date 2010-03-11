90165d9594169494599b18c170d91ec97a0957ca
Ryan Alyn Porter <rap@endymion.com>
2010-03-11 10:29:43 -0500

    Split the branding into two, one for menupride.com, one for menuguilt.com.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def header_image
+     image_file = case request.host
+         when /pride/i
+           'pride-header.gif'
+         else
+           'guilt-header.gif'
+       end
+    image_tag image_file
+  end
 end
