d808b21d23ea737139fa86d33052f933cae0b9a2
Ryan Alyn Porter <rap@endymion.com>
2010-03-11 14:25:35 -0500

    Made page title dynamic also, keyed off of the domain name.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,16 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def page_title
+    base = case request.host
+      when /pride/i
+        'Menu Pride'
+      else
+        'Menu Guilt'
+    end
+    base + ' - Sustainability report cards for seafood restaurants'
+  end
+
   def header_image
      image_file = case request.host
          when /pride/i
@@ -9,4 +20,5 @@ module ApplicationHelper
        end
     image_tag image_file
   end
+
 end
