4e7cac1583d02031a3adbbe31802d60dcaf4888c
Ryan Alyn Porter <rap@endymion.com>
2010-03-13 16:36:42 -0500

    Added authentication functionality using Authlogic.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,12 @@ ActionController::Routing::Routes.draw do |map|
   map.home '/:page', :controller => 'pages', :action => 'show',
     :page => / home /x
 
+  map.resource :user_session
+  map.root :controller => "user_sessions", :action => "new" # optional, this just sets the root route
+
+  map.resource :account, :controller => "users"
+  map.resources :users
+
   # Install the default routes as the lowest priority.
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
