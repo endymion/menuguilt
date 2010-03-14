4eb5db664aede5e798386111547fe80ea7de96b2
Ryan Alyn Porter <rap@endymion.com>
2010-03-14 19:33:40 -0400

    Basic Facebook app setup.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
   acts_as_authentic do |c|
     # c.my_config_option = my_value # for available options see documentation in: Authlogic::ActsAsAuthentic
   end
+  def before_connect(facebook_session)
+    self.name = facebook_session.user.name
+  end
 end
